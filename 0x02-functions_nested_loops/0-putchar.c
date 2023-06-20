90e80bdc33487a4a5b57b5e541d6d0b9e18465a8
ayelemas777@gmail.com <ayelemas777@gmail.com>
2023-06-20 08:34:21 -0700

    putchar is challenging me

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
-int main(void) {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line using the _putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
 
