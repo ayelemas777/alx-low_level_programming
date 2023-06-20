8a177f3825f0bccbd14a5f2126c91c794e74372c
ayelemas777@gmail.com <ayelemas777@gmail.com>
2023-06-20 08:12:39 -0700

    function and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,10 @@
 #include "main.h"
 /**
- * main - prints the string "_putchar" from a character array.
+ * main - prints the string "_putchar" followed by new line.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
